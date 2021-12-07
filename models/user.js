0be3b7c950647f96c3198ff1ea6f15333fea6912
Pierre van Male <vmalep@protonmail.com>
2021-12-07 11:59:49 +0100

    debugging creation of user

--- models/user.js
+++ models/user.js
@@ -1,6 +1,23 @@
 const connection = require('../db-config');
 const Joi = require('joi');
 
+const argon2 = require('argon2');
+
+const hashingOptions = {
+  type: argon2.argon2id,
+  memoryCost: 2 ** 16,
+  timeCost: 5,
+  parallelism: 1
+};
+
+const hashPassword = (plainPassword) => {
+  return argon2.hash(plainPassword, hashingOptions);
+};
+
+const verifyPassword = (plainPassword, hashedPassword) => {
+  return argon2.verify(hashedPassword, plainPassword, hashingOptions);
+};
+
 const db = connection.promise();
 
 const validate = (data, forCreation = true) => {
@@ -69,4 +86,6 @@ module.exports = {
   destroy,
   findByEmail,
   findByEmailWithDifferentId,
+  hashPassword,
+  verifyPassword,
 };
