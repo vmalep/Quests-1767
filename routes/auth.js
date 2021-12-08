1e3d45f48e6efdd6dd4b160930135e144c597878
Pierre van Male <vmalep@protonmail.com>
2021-12-08 23:52:49 +0100

    start

--- routes/auth.js
+++ routes/auth.js
@@ -3,20 +3,17 @@ const User = require('../models/user');
 
 authRouter.post('/checkCredentials', (req, res) => {
   const { email, password } = req.body;
-  console.log(`Check credentials for ${email} with password: ${password}...`);
-  User.findByEmail(email)
-    .then((results) => {
-      User.verifyPassword(password, results.hashedPassword)
-        .then((authenticated) => {
-          console.log('authenticated: ' + authenticated);
-          if(authenticated) res.status(200).json('authenticated')
-          else res.status(400).json('rejected');
-        });
-    })
-    .catch((err) => {
-      console.error(err);
-      res.status(401).send('Error retrieving users from database');
-    });
+  User.findByEmail(email).then((user) => {
+    if (!user) res.status(401).send('Invalid credentials');
+    else {
+      User.verifyPassword(password, user.hashedPassword).then(
+        (passwordIsCorrect) => {
+          if (passwordIsCorrect) res.status(200).send('Your credentials are valid !');
+          else res.status(401).send('Invalid credentials');
+        }
+      );
+    }
+  });
 });
 
 module.exports = authRouter;
